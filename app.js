f7c4e8bf725b24f7d1e702c0a8f7eb068ce8fcb6
Omadbek Teshaboev <oteshaboyev8@gmail.com>
2023-04-18 12:23:04 +0500

    Security Level 2 - Encryption

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save();
+    res.render("login");
+});
+app.post("/login", async function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = await User.findOne({email: username});
+
+    if (foundUser && foundUser.password === password) {
+        res.render("secrets");
+    } else {
+        console.log("Incorrect username or password");
+    }
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
