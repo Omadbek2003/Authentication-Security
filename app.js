437754ebc511fec56d648b3856881de2dd17f08f
Omadbek Teshaboev <oteshaboyev8@gmail.com>
2023-04-23 13:20:31 +0500

    Level 4 Security: Salting and Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -38,21 +39,27 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save();
+        res.render("login"); 
     });
-    newUser.save();
-    res.render("login");
 });
 app.post("/login", async function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     const foundUser = await User.findOne({email: username});
 
-    if (foundUser && foundUser.password === password) {
-        res.render("secrets");
+    if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+                res.render("secrets");
+            }
+        });
     } else {
         console.log("Incorrect username or password");
     }
